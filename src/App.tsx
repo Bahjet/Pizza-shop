6669039a152b4337dbec0625c7e9b899c7989dab
Bahjet <bah.jet@hotmail.com>
2024-02-16 20:42:05 -0300

    Inicioando o projeto Pizza-shop

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
+export function App() {
+
+  return (
+    <>
+      <h2>batata</h2>
+      <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
+    </>
+  )
+}
+
