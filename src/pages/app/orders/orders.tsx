1e2acae2443d8a5c38a71a8a4eb96a35eb3c12ad
Bahjet <bah.jet@hotmail.com>
2024-02-25 21:52:27 -0300

    theme dark mode

---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,17 @@
+import { Helmet } from 'react-helmet-async'
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+      </div>
+      <div className="space-y-2.5">
+        <form action="" className="flex items-center gap-2">
+          <span className="text-sm font-semibold">Filtros:</span>
+        </form>
+      </div>
+    </>
+  )
+}
